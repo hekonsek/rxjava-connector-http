f8c5fc65b81a75b8b63cb0ed7234867723378d06
Henryk Konsek <hekonsek@gmail.com>
2017-12-18 20:06:04 +0100

    Fixed license headers.

--- src/main/java/com/github/hekonsek/rxjava/connector/http/HttpSource.java
+++ src/main/java/com/github/hekonsek/rxjava/connector/http/HttpSource.java
@@ -1,5 +1,5 @@
 /**
- * Licensed to the RxJava Connector Kafka under one or more
+ * Licensed to the RxJava Connector HTTP under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
